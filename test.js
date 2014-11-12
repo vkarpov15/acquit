cb74853ffafa52ba658f575a42fd893542a06c61
Shraya Ramani <shraya.ramani@10gen.com>
2014-11-12 13:43:55 -0500

    more tests

--- test.js
+++ test.js
@@ -1,12 +1,12 @@
 var assert = require('assert');
 var fs = require('fs');
-var bddDox = require('./lib');
+var acquit = require('./lib');
 
 describe('Basic functionality', function() {
   it('can provide basic results', function() {
     var contents = fs.readFileSync('./test/data/sample.js').toString();
 
-    var ret = bddDox.parse(contents);
+    var ret = acquit.parse(contents);
 
     //console.log(ret);
     assert.equal(1, ret.length);
@@ -24,7 +24,7 @@ describe('ES6', function() {
   it('can parse ES6 yield keywords', function() {
     var contents = fs.readFileSync('./test/data/es6_sample.js').toString();
 
-    var ret = bddDox.parse(contents);
+    var ret = acquit.parse(contents);
 
     assert.equal(1, ret.length);
     assert.equal('describe', ret[0].type);
@@ -33,21 +33,6 @@ describe('ES6', function() {
     assert.equal('it', ret[0].blocks[0].type);
     assert.equal(1, ret[0].blocks[0].comments.length);
     assert.ok(ret[0].blocks[0].code);
-
-  });
-});
-
-describe('Generating HTML', function(){
-  it('generates documentation for larger sets of tests', function(done){
-    var contents = fs.readFileSync('./test/data/browser_test.js').toString();
-
-    var ret = bddDox.parse(contents);
-    toHTML = bddDox.generateHTML(ret);
-    assert.doesNotThrow( function() {
-        fs.writeFile('./docs/docs.html', toHTML);
-        done()
-      }
-    );
 
   });
 });
