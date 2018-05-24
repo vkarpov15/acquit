9ae7533a03961f5bd0f696f16e3dafecc7ed642c
Valeri Karpov <val@karpov.io>
2018-05-24 17:19:39 -0400

    docs: finish up home page first pass

--- docs/home.js
+++ docs/home.js
@@ -31,4 +31,27 @@ module.exports = props => `
     </div>
   </div>
 </div>
+<div class="boxes">
+  <div id="parse">
+    <h3>Parse Mocha Tests</h3>
+    The core <a href="https://www.npmjs.com/package/acquit">acquit module</a>
+    is a lightweight parser that parses BDD test syntax and comments into a
+    usable format for plugins.
+  </div>
+  <div id="transpile">
+    <h3>Transpile Tests into Markdown</h3>
+    The <a href="https://www.npmjs.com/package/acquit-markdown">acquit-markdown plugin</a>
+    converts BDD test files into <a href="https://en.wikipedia.org/wiki/Markdown">markdown</a>.
+    If you already have BDD tests and comments, acquit-markdown gives you
+    documentation with a single command.
+  </div>
+  <div id="include">
+    <h3>Import Test Code into HTML</h3>
+    The <a href="https://www.npmjs.com/package/acquit-require">acquit-require plugin</a>
+    lets you import the source code of a BDD test into HTML or markdown. Take
+    your existing documentation, move the examples to a test file, and let your
+    test runner verify your examples.
+  </div>
+  <div style="clear:both"></div>
+</div>
 `.trim();
--- docs/layout.js
+++ docs/layout.js
@@ -8,12 +8,55 @@ module.exports = props => `
     <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Inconsolata" rel="stylesheet">
     <link href="/docs/style.css" rel="stylesheet">
+
+    <script type="text/javascript" src="/docs/native.js"></script>
   </head>
 
   <body>
     ${navbar(props)}
     <div class="content">
       ${props.content}
+    </div>
+    <script type="text/javascript">
+      _native.init("CK7DT53Y", {
+        targetClass: 'native-js',
+        display: 'flex',
+        visibleClass: 'native-show'
+      });
+    </script>
+    <div class="native-js">
+      <div class="native-sponsor">Sponsored by #native_company# — Learn More</div>
+      <a href="#native_link#" class="native-flex">
+        <style>
+        .native-js {
+          background: linear-gradient(-30deg, #native_bg_color#E5, #native_bg_color#E5 45%, #native_bg_color# 45%) #fff;
+        }
+        .native-details,
+        .native-sponsor,
+        .native-bsa {
+          color: #native_color# !important;
+        }
+        .native-details:hover {
+          color: #native_color_hover# !important;
+        }
+        .native-cta {
+          color: #native_cta_color#;
+          background-color: #native_cta_bg_color#;
+        }
+        .native-cta:hover {
+          color: #native_cta_color_hover;
+          background-color: #native_cta_bg_color_hover#;
+        }
+        </style>
+        <div class="native-main">
+          <img class="native-img" src="#native_logo#">
+          <div class="native-details">
+            <span class="native-company">#native_title#</span>
+            <span class="native-desc">#native_desc#</span>
+          </div>
+        </div>
+        <span class="native-cta">#native_cta#</span>
+      </a>
     </div>
   </body>
 </html>
